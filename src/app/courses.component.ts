67ebfa123585b523c57d8114af5250f01172d4f5
uzunemre <uzunemre92@gmail.com>
2019-09-20 23:19:26 +0300

    attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,15 +5,25 @@ import { CoursesService } from './courses.service';
 // property binding
 @Component({
     selector: 'courses',
-    template: `
+    /*template: `
     <h2>{{title}}</h2>
     <h2 [textContent]="title"></h2>
     <img src="{{ imageUrl }}"/>
     <img [src]="imageUrl" />
+    `*/
+
+    // attribute binding. td de colspan igib bir özellik yok o yüzden attrbüte olarak eklyoruz
+    template: `
+    <table>
+        <tr>
+            <td [attr.colspan]="colSpan" >Test</td>
+        </tr>
+    </table>
     `
 })
 
 export class CoursesComponent {
     title = "List of courses";
-    imageUrl = "http://lorempixel.com/400/200/"
+    imageUrl = "http://lorempixel.com/400/200/";
+    colSpan = 2;
 }
