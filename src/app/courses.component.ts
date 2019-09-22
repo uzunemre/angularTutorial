29cbd3d4f64689fecddd0db3a695e56ccefbd4a5
uzunemre <emreuzun92@gmail.com>
2019-09-23 00:15:45 +0300

    implement pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -43,18 +43,42 @@ import { CoursesService } from './courses.service';
         <input #email (keyup.enter)="onKeyUp(email.value)"/>
      `*/
 
-    template: `
+    /*template: `
     <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
-  `
+  `*/
+
+    /*template: `
+    {{course.title | uppercase}}<br/>
+    {{course.students | number }}<br/>
+    {{course.rating | number:'1.2-2' }}<br/>
+    {{course.price | currency }}<br/>
+    {{course.releaseDate | date:'shortDate' }}
+    `*/
+
+    template: `
+        {{text | summary:10 }}
+    `
+
 
 })
 
 export class CoursesComponent {
-    title = "List of courses";
+
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    /*title = "List of courses";
     imageUrl = "http://lorempixel.com/400/200/";
     colSpan = 2;
     isActive = false;
-    email = "emreuzun92@gmail.com";
+    email = "emreuzun92@gmail.com";*/
+
+    /*course = {
+        title: "The Complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016, 3, 1)
+    }*/
 
     /*onSave($event) {
         console.log("Button was clicked", $event);
@@ -68,8 +92,8 @@ export class CoursesComponent {
         alert(email);
     }*/
 
-    onKeyUp() {
+    /*onKeyUp() {
         alert(this.email);
-    }
+    }*/
 
 }
---
+++ src/app/summary.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'summary'
+})
+export class SummaryPipe implements PipeTransform {
+
+    transform(value: string, limit?: number) {
+        if (value) {
+            let actualLimit = (limit) ? limit : 50;
+            return value.substring(0, actualLimit) + "...";
+        }
+        else {
+            return null;
+        }
+    }
+
+}
