27af358d521e92490076ebd086b9e2dbbd499d80
uzunemre <emreuzun92@gmail.com>
2019-09-25 00:40:29 +0300

    favorite component

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+export class FavoriteComponent implements OnInit {
+  isFavorite: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    this.isFavorite = !this.isFavorite;
+    console.log(this.isFavorite);
+  }
+}
