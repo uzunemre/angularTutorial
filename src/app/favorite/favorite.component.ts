a04e629f747c0282893be493718bbb62cec9308e
uzunemre <emreuzun92@gmail.com>
2019-09-25 22:18:46 +0300

    syles

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent {
   // is-favorite alias. html sayfasından is-favorite yazarak erişim sağlanabilir. değişken ismi yerine alieas kullanır
   @Input('is-favorite') isFavorite: boolean; // input anatasyonu dışında bir yol daha var componente decorator inputs yapılabilir
 
   @Output('change') change = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
   onClick() {
     this.isFavorite = !this.isFavorite;
-    this.change.emit({ newValue: this.isFavorite });
+    this.change.emit({newValue: this.isFavorite});
   }
 }
 
 export interface FavoriteChangedEventArgs {
-  newValue: boolean
+  newValue: boolean;
 }
