1714db57794c9ff3fcdb9738bb06e62028052720
uzunemre <emreuzun92@gmail.com>
2019-09-26 00:40:10 +0300

    view encapsulation

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angular/core';
 
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css'],
+  styleUrls: ['./favorite.component.css']
+  //encapsulation: ViewEncapsulation.Emulated // css dosyasındaki stiller bu komponente göredir. başka komponent stilleri ile çakışmaz. her tarayıcıda çalışmaz
 })
 export class FavoriteComponent {
   // is-favorite alias. html sayfasından is-favorite yazarak erişim sağlanabilir. değişken ismi yerine alieas kullanır
