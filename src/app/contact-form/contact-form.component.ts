1cc6d1db39af60e011236ebee33724beca6b25a6
uzunemre <emreuzun92@gmail.com>
2019-09-30 23:27:53 +0300

    template driven forms

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,8 +7,17 @@ import {Component} from '@angular/core';
 })
 export class ContactFormComponent {
 
+  contactMethods = [
+    {id: 1, name: 'Email'},
+    {id: 2, name: 'Phone'}
+  ];
+
   log(x) {
     console.log(x);
+  }
+
+  submit(f) {
+    console.log(f);
   }
 
 }
