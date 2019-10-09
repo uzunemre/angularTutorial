05098b8226cfc7d7c1038bd1d47a7ff1a2e6216a
uzunemre <emreuzun92@gmail.com>
2019-10-09 23:05:02 +0300

    Configuring Routes,RouterLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,43 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {GithubFollowersComponent} from './github-followers/github-followers.component';
+import {GithubProfileComponent} from './github-profile/github-profile.component';
+import {PostsComponent} from './posts/posts.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+
+  // spesifik linkler başta olmalı. GithubFollowersComponent altında olsaydı bu url çalışmazıd
+  {
+    path: 'followers/:username',
+    component: GithubProfileComponent
+  },
+
+  {
+    path: 'followers',
+    component: GithubFollowersComponent
+  },
+
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ErrorHandler, NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CoursesComponent} from './courses.component';
@@ -22,6 +21,10 @@ import {PostService} from './services/post.service';
 import {AppErrorHandler} from './common/app-error-handler';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
 import {GithubFollowersService} from './github-followers.service';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HomeComponent} from './home/home.component';
+import {GithubProfileComponent} from './github-profile/github-profile.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -38,7 +41,11 @@ import {GithubFollowersService} from './github-followers.service';
     ContactFormComponent,
     NewCourseFormComponent,
     PostsComponent,
-    GithubFollowersComponent
+    GithubFollowersComponent,
+    NavbarComponent,
+    HomeComponent,
+    GithubProfileComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
